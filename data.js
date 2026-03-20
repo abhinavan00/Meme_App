62ab749940aa3a5b51f8184214383503c390556b
Abhinav Anand <abhinavanand072@gmail.com>
2026-03-20 13:57:04 +0530

    connected radio items to button

--- data.js
+++ data.js
@@ -1,6 +1,4 @@
 const catsData = [
-
-
     {
         emotionTags: ["moody"],
         isGif: false,
@@ -170,71 +168,3 @@ const catsData = [
         alt: "A cat looking sad",
     },
 ]
-
-const emotionRadios = document.getElementById('emotion-radios')
-const getImageBtn = document.getElementById('get-image-btn')
-
-
-emotionRadios.addEventListener('change', highlightCheckedOption);
-
-getImageBtn.addEventListener('click', getMatchingCatsArray);
-
-function highlightCheckedOption(e) {
-    const radioArray = document.getElementsByClassName('radio')
-    for (let radio of radioArray) {
-        radio.classList.remove('highlight')
-    }
-    document.getElementById(e.target.id).parentElement.classList.add('highlight')
-}
-
-/*
-Challenge:
-1. Take control of the gifs only option checkbox.
-2. Set up a const in getMatchingCatsArray to store 
-   a boolean which will be set to true if the 
-   "gifs only" option is checked and false if it's
-   not. (Think what a good name for this const would 
-   be.)
-3. Log it out to check it's working.
-*/
-
-function getMatchingCatsArray() {
-    const selectedEmotion = document.querySelector('input[type="radio"]:checked')
-    if (selectedEmotion) {
-        console.log(selectedEmotion.value);  
-    }  
-}
-
-function getEmotionsArray(cats){
-    const emotionsArray = []
-    for (let cat of cats) {
-        for (let emotion of cat.emotionTags) {
-            if(!emotionsArray.includes(emotion)) {
-                emotionsArray.push(emotion)
-            }
-        }
-    }
-    return emotionsArray;
-}
-
-function renderEmotionsRadios(cats) {
-    let radioItems = ''
-    const emotions = getEmotionsArray(cats)
-    for (let emotion of emotions) {
-        radioItems += `
-                <div class="radio">
-                    <label for='${emotion}'>${emotion}</label>
-                    <input 
-                        type='radio'
-                        id='${emotion}'
-                        value='${emotion}'
-                        name='emotions'
-                    />
-                </div>
-            `
-    }
-    emotionRadios.innerHTML = radioItems
-}
-
-renderEmotionsRadios(catsData)
-
